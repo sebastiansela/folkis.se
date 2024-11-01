ae418f9dea718d0a28c82a15ccb127e0065c7102
sebastiansela <112576272+sebastiansela@users.noreply.github.com>
2024-11-01 19:25:49 +0100

    Adding shell for site
    #1

---
+++ folkis.js
@@ -0,0 +1,20 @@
+
+$(document).ready(function(){
+    $('.ui.dropdown')
+  .dropdown()
+;
+  });
+
+
+$(document).ready(function(){
+    // All your normal JS code goes in here
+    $(".rating").rating('disable');
+});
+
+$('#dropdown-id').dropdown('setting', 'onChange', function(){
+
+});
+
+function sortByType(){
+
+}
